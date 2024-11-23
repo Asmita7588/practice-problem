abea912ae1f8e7f0d46687327fe011c0f4a621cb
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2024-11-23 09:55:08 +0530

    Initial Changes

---
+++ src/Deposit.java
@@ -0,0 +1,13 @@
+public class Deposit {
+    private double balance;
+
+    // Constructor to initialize balance
+    public Deposit(double initialBalance) {
+        if (initialBalance < 0) {
+            System.out.println("Initial balance cannot be negative. Setting balance to 0.");
+            this.balance = 0;
+        } else {
+            this.balance = initialBalance;
+        }
+    }
+}
